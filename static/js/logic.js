3b95b9800a98a398495a482a0d58895d3ff67679
Ronald Manipol <ronaldmanipol@Tiffanys-MacBook-Air.local>
2021-05-03 00:53:08 +0200

    adding tectonic layer as a overlay along with earthquake layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -101,9 +101,27 @@ d3.json(link).then(function(earthquake_data) {
 }
 
 
-function createMap(earthquakes) {
+function createMap(earthquakes, tectonics) {
 
     // Definiing base map layers
+     
+     var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/light-v10",
+      accessToken: API_KEY
+    });
+    
+    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/outdoors-v11",
+      accessToken: API_KEY
+    });
   
     var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -113,45 +131,26 @@ function createMap(earthquakes) {
     });
     
     var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/satellite-v9",
-    accessToken: API_KEY
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/satellite-v9",
+      accessToken: API_KEY
     });
-
-    var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/light-v10",
-    accessToken: API_KEY
-  });
-
-  var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/outdoors-v11",
-    accessToken: API_KEY
-  });
-
   
   
     // Define a baseMaps object to hold our base layers
-   
-   var baseMaps = {
-    "Satellite":satellitemap,
-    "Grayscale":grayscale,
-    "Dark Map": darkmap,
-    "Outdoors":outdoors,
+    var baseMaps = {
+        "Satellite":satellitemap,
+        "Grayscale":grayscale,
+        "Outdoors":outdoors,
+        "Dark Map": darkmap
     };
-
+  
     // Create overlay object to hold our overlay layer
     var overlayMaps = {
+      "Tectonics Plates": tectonics,
       "Earthquakes": earthquakes
     };
   
@@ -161,7 +160,7 @@ function createMap(earthquakes) {
         37.09, -95.71
       ],
       zoom: 4,
-      layers: [satellitemap, earthquakes]
+      layers: [satellitemap, tectonics, earthquakes]
     });
   
     // Create a layer control
@@ -174,26 +173,26 @@ function createMap(earthquakes) {
     // Set up the legend
     var legend = L.control({ position: "bottomleft" });
     legend.onAdd = function() {
-	var div = L.DomUtil.create("div", "info legend");
-	var limits = ["(-10)-10", "10-30", "30-50", "50-70", "70-90", "90+"];
-	var colors = ["#A3F600", "#DCF400", "#F7DB11", "#FDB72A", "#FCA35D", "#FF5F65"];
-	var labels = [];
-
-    // Add a header
-	var legendInfo = "<h4>Earthquake Depth</h4>" 
-
-	div.innerHTML = legendInfo;
-
-	limits.forEach(function(limit, index) {
-	  labels.push("<li style=\"background-color: " + colors[index]+"\">" + limits[index] + "</li>");
-	});
-
-	div.innerHTML += "<ul style=\"text-align: center\">" + labels.join("") + "</ul>";
-	return div;
+      var div = L.DomUtil.create("div", "info legend");
+      var limits = ["(-10)-10", "10-30", "30-50", "50-70", "70-90", "90+"];
+      var colors = ["#A3F600", "#DCF400", "#F7DB11", "#FDB72A", "#FCA35D", "#FF5F65"];
+      var labels = [];
+  
+      // Add a header
+      var legendInfo = "<h4>Earthquake Depth</h4>" 
+  
+      div.innerHTML = legendInfo;
+  
+      limits.forEach(function(limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index]+"\">" + limits[index] + "</li>");
+      });
+  
+      div.innerHTML += "<ul style=\"text-align: center\">" + labels.join("") + "</ul>";
+      return div;
     };
-
+  
     // Adding legend to the map
-        legend.addTo(myMap);
-
+    legend.addTo(myMap);
   }
+  
   
