47086a78eb93b4eee929aa39524403f2c21f8203
Ronald Manipol <ronaldmanipol@Tiffanys-MacBook-Air.local>
2021-05-03 00:13:30 +0200

    creating initial dark layer as base layer and earthquake as overaly

--- static/js/logic.js
+++ static/js/logic.js
@@ -51,3 +51,46 @@ d3.json(link).then(function(earthquake_data) {
 // Sending our earthquakes and tectonic layer to the createMap function
 //createMap(L.layerGroup(earthquakes), tectonics);
 }
+
+function createMap(earthquakes) {
+
+    // Definiing base map layers
+  
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "dark-v10",
+      accessToken: API_KEY
+    });
+
+  
+  
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+        "Dark Map": darkmap
+    };
+  
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+      "Earthquakes": earthquakes
+    };
+  
+    // Create our map, giving it the satellite, earthquakes and tectonic layers to display on load
+    var myMap = L.map("map", {
+      center: [
+        37.09, -95.71
+      ],
+      zoom: 4,
+      layers: [darkmap, earthquakes]
+    });
+  
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+    
+    
+  }
+  
