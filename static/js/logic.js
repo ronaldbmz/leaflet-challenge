2e29df04d2f2814136726e139f6e9d0e63a232ab
Ronald Manipol <ronaldmanipol@Tiffanys-MacBook-Air.local>
2021-05-03 00:41:53 +0200

    added grayscale base layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -92,13 +92,25 @@ function createMap(earthquakes) {
     zoomOffset: -1,
     id: "mapbox/satellite-v9",
     accessToken: API_KEY
+    });
+
+    var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
   });
+
+
   
   
     // Define a baseMaps object to hold our base layers
    
    var baseMaps = {
     "Satellite":satellitemap,
+    "Grayscale":grayscale,
     "Dark Map": darkmap
     };
 
