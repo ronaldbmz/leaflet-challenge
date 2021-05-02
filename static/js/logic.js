4d88081a8f19a4044b152b2498a15ab987eedd45
Ronald Manipol <ronaldmanipol@Tiffanys-MacBook-Air.local>
2021-05-03 00:39:37 +0200

    added statellite base layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -84,14 +84,24 @@ function createMap(earthquakes) {
       id: "dark-v10",
       accessToken: API_KEY
     });
-
+    
+    var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/satellite-v9",
+    accessToken: API_KEY
+  });
   
   
     // Define a baseMaps object to hold our base layers
-    var baseMaps = {
-        "Dark Map": darkmap
+   
+   var baseMaps = {
+    "Satellite":satellitemap,
+    "Dark Map": darkmap
     };
-  
+
     // Create overlay object to hold our overlay layer
     var overlayMaps = {
       "Earthquakes": earthquakes
@@ -103,7 +113,7 @@ function createMap(earthquakes) {
         37.09, -95.71
       ],
       zoom: 4,
-      layers: [darkmap, earthquakes]
+      layers: [satellitemap, earthquakes]
     });
   
     // Create a layer control
@@ -135,7 +145,7 @@ function createMap(earthquakes) {
     };
 
     // Adding legend to the map
-    legend.addTo(myMap);
+        legend.addTo(myMap);
 
   }
   
